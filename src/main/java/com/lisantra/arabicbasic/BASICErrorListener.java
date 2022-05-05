56f7da7fc46bcfc13842eaef97cfa3d5efe36f8b
Michael Gatto <mgatto@vinsuite.com>
2022-05-05 12:05:40 -0700

    print out syntax errors

--- src/main/java/com/lisantra/arabicbasic/BASICErrorListener.java
+++ src/main/java/com/lisantra/arabicbasic/BASICErrorListener.java
@@ -1,8 +1,12 @@
 package com.lisantra.arabicbasic;
 
 import org.antlr.v4.runtime.BaseErrorListener;
+import org.antlr.v4.runtime.Parser;
 import org.antlr.v4.runtime.RecognitionException;
 import org.antlr.v4.runtime.Recognizer;
+
+import java.util.Collections;
+import java.util.List;
 
 public class BASICErrorListener extends BaseErrorListener {
   @Override
@@ -12,5 +16,12 @@ public class BASICErrorListener extends BaseErrorListener {
       int line,
       int column,
       String message,
-      RecognitionException re) {}
+      RecognitionException re) {
+
+    List<String> stack = ((Parser) recognizer).getRuleInvocationStack();
+    Collections.reverse(stack);
+
+    System.err.println("rule stack: " + stack);
+    System.err.println("line " + line + ": " + column + " at " + offendingSymbol + ": " + message);
+  }
 }
